aac417335ef6c37adcc2c11ab59dd5ebdef35d9b
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-02-03 04:25:20 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div className="footer-div">
-                <p>Made by <a href="https://abclinks.vercel.app" target="_blank">uvacoder</a></p>
+                <p>Made by <a href="https://abclinks.vercel.app" target="_blank" rel="noopener noreferrer">uvacoder</a></p>
                 <p>Built with React, Hosted on <a href="https://github.com/uvacoder" target="blank">Github</a></p>
             </div>
         )
